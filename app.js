410b7b47d7151a7e52a1466ff84b448e26b370bd
Asser Hamad <asserhamad@gmail.com>
2020-02-17 13:37:18 +0200

    Damage control

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use('/api/users', usersRouter);
 app.use('/api/projects', projectRouter);
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req,res) =>{
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 app.use((err, req, res, next) => {
   console.log(err.error)
